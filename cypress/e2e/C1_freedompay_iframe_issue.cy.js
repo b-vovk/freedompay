40b9a8205b1f51249c6a931138cbf0d145a3cb81
Bogdan Vovk <bogdan.vovk@sevenrooms.com>
2023-06-26 18:09:30 -0400

    typos

--- cypress/e2e/C1_freedompay_iframe_issue.cy.js
+++ cypress/e2e/C1_freedompay_iframe_issue.cy.js
@@ -1,11 +1,6 @@
 // C1 Freedompay iframe issue
 
-const card_num = "4012 0000 3333 0026";
-const exp_date = "0330";
-const cvv = "123";
-const zip_code = "10003";
-
-describe("Reservation widget - ", () => {
+describe("Freedompay iframe issue - ", () => {
   beforeEach(() => {
     cy.intercept("GET", "**/api-yoa/availability/dates*").as("venue_dates");
 
@@ -45,6 +40,12 @@ describe("Reservation widget - ", () => {
     // At this point when the modal is displayed we should observe two endpoint being triggered as within manual flow begin_payment and init to cardinal
     // No init endpoint is triggered
     // At this point you can enter payment info manually, but no request will ever be made
+    // Test card data 
+    // const card_num = "4012 0000 3333 0026"; 
+    // const exp_date = "0330";
+    // const cvv = "123";
+    // const zip_code = "10003";
+    
     cy.wait(15000);
     cy.pause();
   });
